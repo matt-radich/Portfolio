85eeae0e7169614c0e30bc104c7fb124f3f6457a
Matt Radich <mattradich2509@gmail.com>
2023-04-12 23:02:01 +1200

    about page set up

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -3,7 +3,33 @@ import '../css/about.css'
 
 function About () {
   return (
-    <h1 className="about-title">About Page</h1>
+    <>
+    <h1 className="nameTitle"><u>HI, I'M MATT</u></h1>
+
+    <div className="aboutContainer">
+      <div className="aboutText">
+        <p>I'm a <b>Software Developer</b> based on the Gold Coast.<br/>
+        Driven by a passion for learning and problem solving to create
+        meaningful solutions.
+        </p>
+      </div>
+      
+          
+      
+      <div className="skills">
+        <div className="skill">HTML</div>
+        <div className="skill">CSS</div>
+        <div className="skill">Javascript</div>
+        <div className="skill">React</div>
+        <div className="skill">MySql</div>
+        <div className="skill">Github</div>
+        <div className="skill">Figma</div>
+        <div className="skill">Node</div>
+      </div>
+    </div> 
+    
+    
+    </>
   )
 }
 
